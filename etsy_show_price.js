b3511c91e2734393a5b9cdcb72125788b13cccfd
Zetaphor <zetaphor@gmail.com>
2014-07-23 22:23:53 -0400

    Initial Commit

---
+++ etsy_show_price.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name       Etsy Sold Price Revealer
+// @namespace  http://zetaphor.net/
+// @version    0.1
+// @description  Reveals the price of sold Etsy listings.
+// @match      http://www.etsy.com/listing/*
+// @match      https://www.etsy.com/listing/*
+// @copyright  2014+, Zetaphor
+// @require http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+
+if ($('.shop-bin-header')[0]) {
+    var price = $('meta[property="etsymarketplace:price"]').attr("content");
+    $('#listing-page-cart-inner').prepend("<h1>Sold for "+ price +"</h1>");
+}
